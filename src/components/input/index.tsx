f807b3b2962b8170ee5008af369c5d63a365bf7d
Lucas Gabriel <lgabriel1607@gmail.com>
2026-04-10 14:04:10 -0300

    feat: tela de login

---
+++ src/components/input/index.tsx
@@ -0,0 +1,9 @@
+export function Input() {
+  return (
+    <input
+      className="border border-neutral-400 h-9 rounded-md outline-none px-2 focus:border-neutral-800"
+      type="text"
+      placeholder="Teste Input..."
+    />
+  );
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
 
 export function Login() {
   return (
-    <div>
-      <h1>Página Login</h1>
+    <div className="flex flex-col w-full h-screen items-center justify-center">
+      <Link to="/">
+        {" "}
+        <h1 className="mt-11 text-neutral-900 mb-7 text-5xl">
+          {" "}
+          Lucas
+          <span className="font-bold bg-linear-to-r from-red-500 to-red-800 bg-clip-text text-transparent">
+            Link
+          </span>{" "}
+        </h1>
+      </Link>
+
+      <form className="w-full max-w-xl flex flex-col px-2 gap-3" action="">
+        <Input />
+
+        <button
+          type="submit"
+          className="h-9  bg-red-800 rounded border-0 text-lg font-medium text-neutral-100"
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
-
-
