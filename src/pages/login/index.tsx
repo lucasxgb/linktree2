aa1c334a70f02b3fe23cc56766ab29fb128a0dec
Lucas Gabriel <lgabriel1607@gmail.com>
2026-04-13 14:27:42 -0300

    feat: conectando com o firebase

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/input";
 import { useState } from "react";
+import { auth } from "../../services/firebaseConection";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  function handleSubmit(event: InputEvent) {
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
 
+    if (email === "" || password === "") alert("Preencha todos os campos!");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate("/admin", { replace: true });
+        console.log("Logado com sucesso");
+      })
+      .catch((error) => {
+        console.log("Erro ao fazer login");
+        console.log(error);
+      });
   }
 
   return (
